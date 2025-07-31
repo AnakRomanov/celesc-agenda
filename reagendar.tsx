8f5db677e10b0860424654f04cab8dbb692f0c9b
AnakRomanov <kcelesc@gmail.com>
2025-07-31 15:33:01 -0300

    Delete reagendar.tsx

--- reagendar.tsx
+++
@@ -1,8 +0,0 @@
-export default function Reagendar() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Reagendar Inspeção</h1>
-      <p>Informe seus dados para reagendar sua inspeção.</p>
-    </div>
-  );
-}
