3f9b0900fed12582e3885194024aebc5bdf35e0a
AnakRomanov <kcelesc@gmail.com>
2025-07-31 15:31:51 -0300

    Delete backoffice directory

--- backoffice/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Backoffice() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Área Administrativa - Backoffice</h1>
-      <p>Visualize e gerencie os agendamentos realizados.</p>
-    </div>
-  );
-}
