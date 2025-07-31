7164a3ec13da50424e9335e7cc1f8383cd8f4702
AnakRomanov <kcelesc@gmail.com>
2025-07-31 15:32:29 -0300

    Delete consulta.tsx

--- consulta.tsx
+++
@@ -1,8 +0,0 @@
-export default function Consulta() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Consultar Agendamento</h1>
-      <p>Insira seu CPF ou número de protocolo para consultar o agendamento.</p>
-    </div>
-  );
-}
